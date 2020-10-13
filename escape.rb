6814ed0aa60a747f72d7dee71d5154bfc0ee2db9
Ale Fuentes <jano231982@gmail.com>
2020-10-13 20:43:39 -0300

    primera prueba

---
+++ escape.rb
@@ -0,0 +1,11 @@
+#gravedad = 0
+#radio = 0
+
+print "ingrese la gravedad del planeta"
+gravedad= gets.chomp
+
+print "ingrese el radio del planeta"
+radio = gets.chomp
+
+puts radio 
+print gravedad
